19c643566cd7e7ea4610a158621157dace5aed25
cntprogram <idontgive@emails.com>
2022-03-25 12:36:38 +0800

    aa

--- playground/demo-7/instance.tf
+++ playground/demo-7/instance.tf
@@ -10,4 +10,27 @@ resource "aws_instance" "sample" {
 
     #the public SSH key
     key_name = "${aws_key_pair.mykey.key_name}"
+
+    #expand disk capacity
+    root_block_device {
+      volume_size = 16
+      volume_type = "gp2"
+      delete_on_termination = true #if true the instance is terminated, so is the disk
+    }
+}
+
+resource "aws_ebs_volume" "ebs-volume-1"{
+    availability_zone = "${var.AWS_ZONE["east1a"]}"
+    size = 20
+    type = "gp2" #general purpose storage, standard ssd
+    tags = {
+        Name = "Extra volume data"
+    }
+
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+    device_name = "/dev/xvdh"
+    volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
+    instance_id = "${aws_instance.sample.id}"
 }
