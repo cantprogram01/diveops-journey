754045f6cb47b489ee0efb7273f15856e8af1658
arwinvincent <arwinvincent>
2022-04-03 09:49:37 +0800

    cleaning up

--- scriptfiles/lamp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#To install the default LAMP stack in Ubuntu 10.04 and above
-echo "INSTALLING LAMP for Ubuntu 10.04 and above only..."
-
-sudo apt-get update -y
-sudo apt-get install lamp-server^ -y
-
--- scriptfiles/tomcat.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-sudo -i
-docker pull centos
